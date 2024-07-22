ab7405f78acbb8e7f0c39093361c8e3eb5808760
Sl-Maria <168527483+Sl-Maria@users.noreply.github.com>
2024-07-22 18:46:56 +0300

    VD05_templates

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ def index():
 def blog():
     return render_template("blog.html")
 
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
 @app.route("/contacts/")
 def contacts():
     return render_template("contacts.html")
